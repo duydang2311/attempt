8e269f0d20308122a4753e210aab8d57eab0967a
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-10-02 05:16:30 +0700

    feat: make flatMap, map function-first; add attempt.pipe

--- lib/attempt.ts
+++ lib/attempt.ts
@@ -1,18 +1,89 @@
+import __pipe from '@bitty/pipe';
+
 const symbol = Symbol('attempt');
 
-export type Attempt<A, E> =
-    | {
-          readonly [symbol]: true;
-          readonly ok: true;
-          readonly failed: false;
-          readonly data: A;
-      }
-    | {
-          readonly [symbol]: true;
-          readonly ok: false;
-          readonly failed: true;
-          readonly error: E;
-      };
+export interface Ok<A> {
+    readonly ok: true;
+    readonly failed: false;
+    readonly data: A;
+}
+
+export interface Fail<E> {
+    readonly ok: false;
+    readonly failed: true;
+    readonly error: E;
+}
+
+export type Attempt<A, E> = (Ok<A> | Fail<E>) & {
+    pipe<T1>(...fns: [(a: Attempt<A, E>) => T1]): T1;
+    pipe<T1, T2>(...fns: [(a: Attempt<A, E>) => T1, (a: T1) => T2]): T2;
+    pipe<T1, T2, T3>(
+        ...fns: [(a: Attempt<A, E>) => T1, (a: T1) => T2, (a: T2) => T3]
+    ): T3;
+    pipe<T1, T2, T3, T4>(
+        ...fns: [
+            (a: Attempt<A, E>) => T1,
+            (a: T1) => T2,
+            (a: T2) => T3,
+            (a: T3) => T4,
+        ]
+    ): T4;
+    pipe<T1, T2, T3, T4, T5>(
+        ...fns: [
+            (a: Attempt<A, E>) => T1,
+            (a: T1) => T2,
+            (a: T2) => T3,
+            (a: T3) => T4,
+            (a: T4) => T5,
+        ]
+    ): T5;
+    pipe<T1, T2, T3, T4, T5, T6>(
+        ...fns: [
+            (a: Attempt<A, E>) => T1,
+            (a: T1) => T2,
+            (a: T2) => T3,
+            (a: T3) => T4,
+            (a: T4) => T5,
+            (a: T5) => T6,
+        ]
+    ): T6;
+    pipe<T1, T2, T3, T4, T5, T6, T7>(
+        ...fns: [
+            (a: Attempt<A, E>) => T1,
+            (a: T1) => T2,
+            (a: T2) => T3,
+            (a: T3) => T4,
+            (a: T4) => T5,
+            (a: T5) => T6,
+            (a: T6) => T7,
+        ]
+    ): T7;
+    pipe<T1, T2, T3, T4, T5, T6, T7, T8>(
+        ...fns: [
+            (a: Attempt<A, E>) => T1,
+            (a: T1) => T2,
+            (a: T2) => T3,
+            (a: T3) => T4,
+            (a: T4) => T5,
+            (a: T5) => T6,
+            (a: T6) => T7,
+            (a: T7) => T8,
+        ]
+    ): T8;
+    pipe<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+        ...fns: [
+            (a: Attempt<A, E>) => T1,
+            (a: T1) => T2,
+            (a: T2) => T3,
+            (a: T3) => T4,
+            (a: T4) => T5,
+            (a: T5) => T6,
+            (a: T6) => T7,
+            (a: T7) => T8,
+            (a: T8) => T9,
+        ]
+    ): T9;
+};
 
 interface AttemptAsyncFn {
     <A, E>(fn: () => Promise<Attempt<A, E>>): <TError2>(
@@ -54,36 +125,60 @@ export const attemptAsync: AttemptAsyncFn = <A, E>(
 };
 
 export const attemptOk = <A>(data: A): Attempt<A, never> => ({
-    [symbol]: true,
     ok: true,
     failed: false,
     data,
+    pipe,
 });
 
 export const attemptFail = <E>(error: E): Attempt<never, E> => ({
-    [symbol]: true,
     ok: false,
     failed: true,
     error,
+    pipe,
 });
 
-export const attemptMap =
-    <A, E>(attempt: Attempt<A, E>) =>
-    <B>(f: (a: A) => B): Attempt<B, E> => {
-        if (attempt.ok) {
-            return attemptOk(f(attempt.data));
+export function attemptMap<A, E, A2>(
+    f: (a: A) => Promise<A2>,
+): (attempt: Attempt<A, E>) => Promise<Attempt<A2, E>>;
+export function attemptMap<A, E, A2>(
+    f: (a: A) => A2,
+): (attempt: Attempt<A, E>) => Attempt<A2, E>;
+export function attemptMap<A, E, A2>(
+    f: (a: A) => Promise<A2> | A2,
+): (attempt: Attempt<A, E>) => Promise<Attempt<A2, E>> | Attempt<A2, E> {
+    return (
+        attempt: Attempt<A, E>,
+    ): Promise<Attempt<A2, E>> | Attempt<A2, E> => {
+        if (attempt.failed) {
+            return attempt as Attempt<A2, E>;
         }
-        return attempt;
+        const ret = f(attempt.data);
+        if (ret instanceof Promise) {
+            return ret.then(attemptOk);
+        }
+        return attemptOk(ret);
     };
+}
 
-export const attemptFlatMap =
-    <A, E>(attempt: Attempt<A, E>) =>
-    <A2, E2>(f: (a: A) => Attempt<A2, E2>): Attempt<A2, E | E2> => {
-        if (attempt.ok) {
-            return f(attempt.data);
+export function attemptFlatMap<A, A2, E2>(
+    f: (a: A) => Promise<Attempt<A2, E2>>,
+): <E>(attempt: Attempt<A, E>) => Promise<Attempt<A2, E | E2>>;
+export function attemptFlatMap<A, A2, E2>(
+    f: (a: A) => Attempt<A2, E2>,
+): <E>(attempt: Attempt<A, E>) => Attempt<A2, E | E2>;
+export function attemptFlatMap<A, A2, E2>(
+    f: (a: A) => Attempt<A2, E2> | Promise<Attempt<A2, E2>>,
+): <E>(
+    attempt: Attempt<A, E>,
+) => Attempt<A2, E | E2> | Promise<Attempt<A2, E | E2>> {
+    return <E>(attempt: Attempt<A, E>) => {
+        if (attempt.failed) {
+            return attempt as Attempt<A2, E | E2>;
         }
-        return attemptFail(attempt.error);
+        return f(attempt.data);
     };
+}
 
 export const attempt = {
     sync: attemptSync,
@@ -97,3 +192,7 @@ export const attempt = {
 const isAttempt = <A, E>(value: any): value is Attempt<A, E> => {
     return typeof value === 'object' && value !== null && symbol in value;
 };
+
+function pipe(this: unknown, ...fns: Function[]) {
+    return __pipe(() => this, ...(fns as []))();
+}
